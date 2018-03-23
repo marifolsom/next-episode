8da1f6639121ef43a1aef233aa2b50e025d1a344
marifolsom <mfolsom1@fordham.edu>
2018-03-22 23:47:36 -0400

    added getPopular, getTopRated, getAiring functions

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ const getJSON = url => fetch(url).then(response => response.json())
 
 // TMDB API
 // Get tv show's id by keyword
-const getShow = (searchQuery) => {
+const getShowId = (searchQuery) => {
   // Make an API request with the user's inputted keywords
   getJSON(`https://api.themoviedb.org/3/search/tv?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US&query=${searchQuery}`)
   .then(showData => {
@@ -38,7 +38,7 @@ const getShow = (searchQuery) => {
     console.log('show id:', showId);
   })
 }
-getShow(searchQuery);
+getShowId(searchQuery);
 
 
 // Get tv show details by show id
@@ -54,6 +54,7 @@ const getShowInfo = (showId) => {
     const numberOfSeasons = showData.number_of_seasons;
     const numberOfEpisodes = showData.number_of_episodes;
     const showRating = showData.vote_average;
+    const showPopularity = showData.popularity;
     const showStatus = showData.status;
     const showSite = showData.homepage;
     const showImg = showData.backdrop_path;
@@ -63,16 +64,17 @@ const getShowInfo = (showId) => {
     console.log('seasons:', numberOfSeasons);
     console.log('episodes:', numberOfEpisodes);
     console.log('show rating:', showRating);
+    console.log('show popularity:', showPopularity);
     console.log('show status:', showStatus);
     console.log('show url:', showSite);
     console.log('show img:', showImg);
     console.log('-------------------------------');
     for (let i = 0; i < showData.seasons.length; i++) {
       const seasonNumber = showData.seasons[i].season_number;
+      const seasonTitle = showData.seasons[i].name;
       const dates = showData.seasons[i].air_date.split('-');
       const seasonAirdate = `${dates[1]}/${dates[2]}/${dates[0]}`;
       const episodeCount = showData.seasons[i].episode_count;
-      const seasonTitle = showData.seasons[i].name;
       const seasonImg = showData.seasons[i].poster_path;
       const seasonLink = `https://api.themoviedb.org/3/tv/${showId}/season/${seasonNumber}?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`;
       console.log('season title:', seasonTitle);
@@ -113,25 +115,27 @@ getSeason(showId, 3)
 
 
 // Get show recommendations by ID
-const getRecommendations = () => {
+const getRecs = () => {
   // Make an API request with a show id
   getJSON(`https://api.themoviedb.org/3/tv/${showId}/recommendations?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`)
-  .then(recommendations => {
-    for (let i = 0; i < recommendations.results.length; i++) {
-      const recId = recommendations.results[i].id
-      const recTitle = recommendations.results[i].name;
-      const recDesc = recommendations.results[i].overview;
-      const recRating = recommendations.results[i].vote_average;
+  .then(recsData => {
+    for (let i = 0; i < recsData.results.length; i++) {
+      const recId = recsData.results[i].id
+      const recTitle = recsData.results[i].name;
+      const recDesc = recsData.results[i].overview;
+      const rating = recsData.results[i].vote_average;
+      const recImg = recsData.results[i].poster_path;
       const recLink = `https://api.themoviedb.org/3/tv/${recId}?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`;
       console.log('rec title:', recTitle);
       console.log('rec desc:', recDesc);
-      console.log('rec rating:', recRating);
+      console.log('rec rating:', rating);
+      console.log('rec img:', recImg);
       console.log('rec link:', recLink);
       console.log('-------------------------------');
     }
   })
 }
-getRecommendations();
+getRecs();
 
 
 // Get most popular shows
@@ -139,7 +143,22 @@ const getPopular = () => {
   // Make API request
   getJSON(`https://api.themoviedb.org/3/tv/popular?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`)
   .then(popularData => {
-
+    for (let i = 0; i < popularData.results.length; i++) {
+      const popId = popularData.results[i].id;
+      const popTitle = popularData.results[i].name;
+      const popDesc = popularData.results[i].overview;
+      const rating = popularData.results[i].vote_average;
+      const popularity = popularData.results[i].popularity;
+      const popImg = popularData.results[i].poster_path;
+      const popLink = `https://api.themoviedb.org/3/tv/${popId}?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`;
+      console.log('pop title:', popTitle);
+      console.log('pop desc:', popDesc);
+      console.log('rating:', rating);
+      console.log('popularity:', popularity);
+      console.log('pop img:', popImg);
+      console.log('pop link:', popLink);
+      console.log('-------------------------------');
+    }
   })
 }
 getPopular();
@@ -150,7 +169,22 @@ const getTopRated = () => {
   // Make API request
   getJSON(`https://api.themoviedb.org/3/tv/top_rated?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`)
   .then(topRatedData => {
-
+    for (let i = 0; i < topRatedData.results.length; i++) {
+      const topId = topRatedData.results[i].id;
+      const topTitle = topRatedData.results[i].name;
+      const topDesc = topRatedData.results[i].overview;
+      const rating = topRatedData.results[i].vote_average;
+      const popularity = topRatedData.results[i].popularity;
+      const topImg = topRatedData.results[i].poster_path;
+      const topLink = `https://api.themoviedb.org/3/tv/${topId}?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`;
+      console.log('top title:', topTitle);
+      console.log('top desc:', topDesc);
+      console.log('rating:', rating);
+      console.log('popularity:', popularity);
+      console.log('top img:', topImg);
+      console.log('top link:', topLink);
+      console.log('-------------------------------');
+    }
   })
 }
 getTopRated();
@@ -161,7 +195,22 @@ const getAiring = () => {
   // Make API request
   getJSON(`https://api.themoviedb.org/3/tv/airing_today?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`)
   .then(airingData => {
-
+    for (let i = 0; i < airingData.results.length; i++) {
+      const airingId = airingData.results[i].id;
+      const airingTitle = airingData.results[i].name;
+      const airingDesc = airingData.results[i].overview;
+      const rating = airingData.results[i].vote_average;
+      const popularity = airingData.results[i].popularity;
+      const airingImg = airingData.results[i].poster_path;
+      const airingLink = `https://api.themoviedb.org/3/tv/${airingId}?api_key=085991675705d18c9d1f19c89cae4e50&language=en-US`;
+      console.log('airing title:', airingTitle);
+      console.log('airing desc:', airingDesc);
+      console.log('rating:', rating);
+      console.log('popularity:', popularity);
+      console.log('airing img:', airingImg);
+      console.log('airing link:', airingLink);
+      console.log('-------------------------------');
+    }
   })
 }
 getAiring();
